050b097eefd1ea8b2d0133b780b6afc4465c0278
123 <salihovtahir2007@gmail.com>
2022-01-28 20:13:25 +0500

    release v-1

---
+++ compiler/984500e9-8221-4365-a4d5-2e4fe9c6af5f.bat
@@ -0,0 +1,2 @@
+python  C:\Users\123\Downloads\pyCompiler\compiler\984500e9-8221-4365-a4d5-2e4fe9c6af5f.py 
+exit
---
+++ src/main/Main.java
@@ -0,0 +1,80 @@
+package main;
+
+import java.io.*;
+import java.util.Formatter;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class Main {
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String code = readConsole();
+
+        String fileName = UUID.randomUUID().toString();
+        String pathToFiles = "C:\\Users\\123\\Downloads\\pyCompiler\\compiler\\";
+        File pythonFile = new File(pathToFiles + fileName + ".txt");
+        pythonFile.createNewFile();
+        Formatter formatter = new Formatter(pathToFiles + fileName + ".txt");
+        formatter.format("%s", code);
+        formatter.close();
+        pythonFile.renameTo(new File(pathToFiles + fileName + ".py"));
+
+        File bat = new File(pathToFiles + fileName + ".txt");
+        bat.createNewFile();
+        Formatter formatter2 = new Formatter(pathToFiles + fileName + ".txt");
+        String command =
+                "python  " + pathToFiles + fileName + ".py \n" +
+                "exit";
+        formatter2.format("%s", command);
+        formatter2.close();
+        bat.renameTo(new File(pathToFiles + fileName + ".bat"));
+
+        ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "call", pathToFiles + fileName + ".bat");
+        pb.redirectErrorStream(true);
+        pb.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        Process process = pb.start();
+        printStream(process.getErrorStream(), process.getOutputStream());
+        printStream(process.getInputStream(), process.getOutputStream());
+    }
+
+    public static String readFile(File outTextFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(outTextFile);
+        StringBuilder out = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            out.append(scanner.nextLine());
+        }
+        return out.toString();
+    }
+
+
+    public static String readConsole() throws IOException {
+        System.out.println("----------------------------");
+        StringBuilder stringBuilder = new StringBuilder();
+        while (true) {
+            String s = reader.readLine();
+            if (s.equals("stop")) break;
+            stringBuilder.append(s).append("\n");
+        }
+        return  stringBuilder.toString();
+    }
+
+    public static void printToConsole(OutputStream out, String s) throws IOException {
+        OutputStreamWriter outr = new OutputStreamWriter(out);
+        BufferedWriter bw = new BufferedWriter(outr);
+        bw.write(s);
+    }
+
+    public static void printStream(InputStream is, OutputStream out) throws IOException {
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        int line;
+        while ((line = br.read()) != -1) {
+            System.out.print((char) line);
+        }
+    }
+
+}
+
+
